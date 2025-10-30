5baf9badb3930da3c478e364dcb1d5a8e77960e2
rafay4ptek <rafay@4ptek.com>
2025-10-30 23:32:27 +0500

    admin and role table add

---
+++ src/entities/entities/Admin.ts
@@ -0,0 +1,47 @@
+import { Column, Entity, Index, JoinColumn, ManyToOne } from "typeorm";
+import { Role } from "./Role";
+
+@Index("UQ_386657905f0fdeabc53555beba3", ["email"], { unique: true })
+@Index("admin_pkey", ["id"], { unique: true })
+@Entity("admin", { schema: "public" })
+export class Admin {
+  @Column("integer", { primary: true, name: "id" })
+  id: number;
+
+  @Column("character varying", { name: "first_name", length: 255 })
+  firstName: string;
+
+  @Column("character varying", { name: "last_name", length: 255 })
+  lastName: string;
+
+  @Column("bigint", { name: "contact", nullable: true })
+  contact: string | null;
+
+  @Column("character varying", { name: "email", unique: true, length: 255 })
+  email: string;
+
+  @Column("character varying", { name: "hashed_password", length: 255 })
+  hashedPassword: string;
+
+  @Column("boolean", { name: "is_deactive", default: () => "false" })
+  isDeactive: boolean;
+
+  @Column("boolean", { name: "is_delete", default: () => "false" })
+  isDelete: boolean;
+
+  @Column("boolean", { name: "is_firstlogin", default: () => "true" })
+  isFirstlogin: boolean;
+
+  @Column("timestamp without time zone", { name: "invited_at", nullable: true })
+  invitedAt: Date | null;
+
+  @Column("timestamp without time zone", { name: "deleted_at", nullable: true })
+  deletedAt: Date | null;
+
+  @Column("timestamp without time zone", { name: "created_at", nullable: true })
+  createdAt: Date | null;
+
+  @ManyToOne(() => Role, (role) => role.admins, { onDelete: "CASCADE" })
+  @JoinColumn([{ name: "role_id", referencedColumnName: "id" }])
+  role: Role;
+}
---
+++ src/entities/entities/Role.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, Index, OneToMany } from "typeorm";
+import { Admin } from "./Admin";
+
+@Index("role_pkey", ["id"], { unique: true })
+@Index("role_name_key", ["name"], { unique: true })
+@Entity("role", { schema: "public" })
+export class Role {
+  @Column("integer", { primary: true, name: "id" })
+  id: number;
+
+  @Column("character varying", { name: "name", unique: true, length: 50 })
+  name: string;
+
+  @Column("timestamp without time zone", {
+    name: "created_at",
+    default: () => "now()",
+  })
+  createdAt: Date;
+
+  @OneToMany(() => Admin, (admin) => admin.role)
+  admins: Admin[];
+}
