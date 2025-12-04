82f3a1e667b103913eb9b71c9e38c19e39903712
Jawwad-Ahmed-Khan <jawwadahmedkhan63@gmail.com>
2025-12-05 00:27:57 +0500

    Sweggar add

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({transform: true,whitelist: true,forbidNonWhitelisted: true}));
+
+   const config = new DocumentBuilder()
+        .setTitle('Your API Title') // Customize your API title
+        .setDescription('Your API description') // Customize your API description
+        .setVersion('1.0') // Customize your API version
+        .addBearerAuth() // Optional: if you use bearer token authentication
+        .build();
+
+      const document = SwaggerModule.createDocument(app, config);
+      SwaggerModule.setup('api', app, document); // 'api' is the path where Swagger UI will be accessible
+
+
   await app.listen(3005);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
